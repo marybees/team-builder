35073bb404f2d0583e8be7bd0c6a7e3b91be47d6
Mary Baker <meb601@gmail.com>
2020-08-18 18:38:49 -0400

    Add Form.js, basic structure and input field

---
+++ team-builder/src/Form.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Form = props => {
+    return (
+        <form>
+            <label htmlFor='title'></label>
+            <input id = 'title' type='text'/>
+        </form>
+    )
+};
+
+export default Form
