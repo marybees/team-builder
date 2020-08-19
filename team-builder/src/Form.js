7bd8ae3edc043ffc089a6b90e8e1bc6a34fab373
Mary Baker <meb601@gmail.com>
2020-08-18 21:17:20 -0400

    Populate Form.js

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 
 const Form = props => {
+    const [form, setForm] = useState({
+        title: ''
+    });
+    const handleChanges = e => {
+        setForm({title: e.target.value})
+    }
     return (
         <form>
             <label htmlFor='title'></label>
-            <input id = 'title' type='text'/>
+            <input id = 'title' type='text' onChange={handleChanges}/>
         </form>
     )
 };
